ac7a0513eaed5443e70439d68dd2e0a8879fdc1d
cristian <ceparadac>
2015-06-14 01:05:46 -0500

    --Example Kubuntu

--- UNAirlines2.0/grails-app/conf/BootStrap.groovy
+++ UNAirlines2.0/grails-app/conf/BootStrap.groovy
@@ -10,6 +10,8 @@ class BootStrap {
             u.save();
         def u2 = new Admin(name:"admin2",surname:"admin2",email:"admin2@unal.edu.co",password:"12345")
             u2.save();
+        def u3 = new Admin(name:"admin3",surname:"admin3",email:"admin3@unal.edu.co",password:"12345")
+            u3.save();
         def m = new Customer(name:"user",surname:"user",email:"user@unal.edu.co",password:"12345",phone:"1234567",address:"Cra 45 # 56-54")
             m.save();
         
